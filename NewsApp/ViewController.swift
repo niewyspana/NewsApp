047b32be7f1c3c0d32232a257a104c0e5e3eac3e
niewyspana <miss.polazava@gmail.com>
2023-12-05 18:05:21 +0100

    add new files

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Viki on 05/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
